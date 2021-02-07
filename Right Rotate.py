8e66c07b0742dbe600b9d32177ff0b8326f7155b
Purna <yeluripurnasairam@gmail.com>
2021-02-07 13:41:21 +0530

    Test Push

---
+++ Right Rotate.py
@@ -0,0 +1,9 @@
+lst = [10,20,30,40,50]
+n = int(input("Enter the rotation: "))
+print("Original List is:",lst)
+l = len(lst)
+l1 = lst[0:l-n]
+l2 = lst[l-n:l]
+lst2 = l2+l1
+
+print("{} rotated list is {}".format(n,lst2))
